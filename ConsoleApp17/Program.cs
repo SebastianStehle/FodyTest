b269f2357e9e95b14b00f8e88bd85dacef67a9f0
Sebastian <sebastian@squidex.io>
2020-01-30 13:18:45 +0100

    Fody test

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        class Base
+        {
+            [IgnoreDuringEquals]
+            public int Bar { get; set; }
+        }
+
+        [Equals(DoNotAddEqualityOperators = true)]
+        class MyEquals : Base
+        {
+            public int Foo { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var type = typeof(MyEquals);
+
+            var properties = type.GetProperties();
+
+            foreach (var property in properties)
+            {
+                try
+                {
+                    var attributes = property.GetCustomAttributes();
+
+                    Console.WriteLine($"Succes: {property.Name}");
+                }
+                catch
+                {
+                    Console.WriteLine($"Failed: {property.Name}");
+                }
+            }
+        }
+    }
+}
